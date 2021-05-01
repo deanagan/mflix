209db3a22f547829612b33d87bccecad22224942
Dean Agan <agandfr@gmail.com>
2021-05-01 21:20:01 +1000

    Facet test and fixed paging solution

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -196,6 +196,9 @@ export default class MoviesDAO {
     const queryPipeline = [
       matchStage,
       sortStage,
+      skipStage,
+      limitStage,
+      facetStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
     ]
@@ -261,7 +264,8 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage).skip(moviesPerPage * page)
+
+    const displayCursor = cursor.skip(moviesPerPage * page).limit(moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()
